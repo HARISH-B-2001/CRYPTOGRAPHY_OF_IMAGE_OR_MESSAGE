24770894d2feab1c2d7d291c53dbe296126febea
HARISH B <72798342+HARISH-B-2001@users.noreply.github.com>
2021-02-06 09:40:49 +0530

    Update encrypt_decrypt_message.py

--- encrypt_decrypt_message.py
+++ encrypt_decrypt_message.py
@@ -1,17 +1,17 @@
-from cryptography.fernet import Fernet
-def key():
+from cryptography.fernet import Fernet                              #import cryptography module
+def key():                                                          #generate key
     key = Fernet.generate_key()
     return key
 
-def encryption(text,key):
-    encoded_message = text.encode()
+def encryption(text,key):                                           
+    encoded_message = text.encode()                                #encode the message
     f = Fernet(key)
-    encrypted_message = f.encrypt(encoded_message)
+    encrypted_message = f.encrypt(encoded_message)                 #encrypting the message
     return encrypted_message
 
 def decryption(encrypted_message,key):
     f = Fernet(key)
-    decrypted_message = (f.decrypt(encrypted_message)).decode()
+    decrypted_message = (f.decrypt(encrypted_message)).decode()    #decrypting and decode the message
     return decrypted_message
 
 if __name__ == '__main__':

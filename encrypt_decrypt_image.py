0e267eada62223f4ed1f7bc5be941bcedbeff84c
HARISH B <72798342+HARISH-B-2001@users.noreply.github.com>
2021-02-06 09:37:36 +0530

    Update encrypt_decrypt_image.py

--- encrypt_decrypt_image.py
+++ encrypt_decrypt_image.py
@@ -1,22 +1,22 @@
 def encrypt(adress,key):
-    fo = open(adress , "rb")
+    fo = open(adress , "rb")                    #open Image as fo
     image = fo.read()
     fo.close()
-    image = bytearray(image)
+    image = bytearray(image)                    #convert image into bytearray
     for index, value in enumerate(image):
         image[index] = value^int(key)
-    fo = open("enc.jpg" ,"wb" )
+    fo = open("enc.jpg" ,"wb" )                 #write and save encrypted image as enc.jpg
     fo.write(image)
     fo.close()
 
 def decrypt(adress,key):
-    fo = open(adress, "rb")
+    fo = open(adress, "rb")                     #open encrypted image as fo
     image = fo.read()
     fo.close()
     image = bytearray(image)
     for index, value in enumerate(image):
         image[index] = value ^int(key)
-    fo = open("dec.jpg", "wb")
+    fo = open("dec.jpg", "wb")                  #convert and save decryptedimage as dec.jpg
     fo.write(image)
     fo.close()
 
